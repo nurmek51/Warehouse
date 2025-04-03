3084bc82e35268ec4a70a057919535f8fbbaeb93
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-04-03 19:35:22 +0500

    Added initial models for store app

--- store/models.py
+++ store/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class StoreItem(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, blank=True, null=True)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    expire_date = models.DateField()
+    status = models.CharField(max_length=50, default='active')
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
