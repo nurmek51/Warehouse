ab0355daadec95c01b7a858a524a6fbe2cb27f8e
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-04-03 16:09:41 +0500

    Warehouse project initialized

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'warehouse.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+#
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
