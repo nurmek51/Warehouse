f7edeaadd4984c231a79702389aa85e7b2d0be0e
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-04-04 23:58:33 +0500

    Added tools for admin. Added account tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import User
 
-# Create your tests here.
+class AccountsTests(APITestCase):
+    def test_registration(self):
+        url = reverse('register')
+        data = {
+            'email': 'maxsatul2007@gmail.com',
+            'password': 'strong_password_123',
+            'username': 'testuser'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(User.objects.filter(email='maxsatul2007@gmail.com').exists())
