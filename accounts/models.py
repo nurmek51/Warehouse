7fd27b99627fbcb97e1cea9d5d079636cba518e4
maksatuly nurmuhamed <nurmeksdu@gmail.com>
2025-04-05 01:58:29 +0500

    Added permissions logic. New permissions: Admin, Manager, Client.

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_verified = models.BooleanField(default=False)
     verification_code = models.CharField(max_length=64, blank=True, null=True)
+
+    ROLE_CHOICES = (
+        ('admin', 'Administrator'),
+        ('manager', 'Manager'),
+        ('customer', 'Client'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
